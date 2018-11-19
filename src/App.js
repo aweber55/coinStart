22882b0c86c0e2d9098bd3a6a2b7bdc76dedaeb2
Ashley Weber <aweber333@yahoo.com>
2018-11-19 14:32:44 -0500

    background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+// import { Nav } from 'react-bootstrap';
+import Nav from "./components/Nav";
 class App extends Component {
   render() {
     return (
+      <div className="bodyOne">
+      <Nav/>
+     
+
       <div className="App">
+      {/* <Row> */}
         <header className="App-header">
+        {/* <Col size="md-12"> */}
           <img src={logo} className="App-logo" alt="logo" />
+          {/* </Col> */}
+         
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <button type="button" className="btn btn-danger">Primary</button>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -19,7 +32,10 @@ class App extends Component {
           >
             Learn React
           </a>
+         <a href="/Home">home</a>
         </header>
+        {/* </Row> */}
+      </div>
       </div>
     );
   }
